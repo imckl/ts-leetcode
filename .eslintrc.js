419f5a7b0c3a73bbeec23b58b52106823ff198b7
imckl <imckl@outlook.com>
2020-11-23 10:21:39 +0800

    update: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        quotes: ['error', 'single', {'avoidEscape': true}],
+        strict: 'error',
+        semi: 'error',
+        eqeqeq: 'error'
+    },
+    env: {
+        node: true
+    }
+};
